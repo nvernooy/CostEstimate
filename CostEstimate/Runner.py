a30d9fd113e95917060465c3d10d8b6f0d984fb5
Niel Vernooy <vernooy.n@gmail.com>
2014-09-25 00:09:29 +0200

    Working version
    Basic functions seem to work. So far only adding one instance of each
    object with no further operations.

--- CostEstimate/Runner.py
+++ CostEstimate/Runner.py
@@ -1,6 +1,7 @@
-from BTrees.OOBTree import BTree
-from models import Project
-import ZODB, ZODB.FileStorage, Project, transation
+from BTrees.OOBTree import OOBTree
+from models import Project, BudgetGroup, BudgetItem 
+import ZODB, ZODB.FileStorage
+import transaction
 
 if __name__ == "__main__":
     # create db connection
@@ -9,9 +10,56 @@ if __name__ == "__main__":
     connection = db.open()
     root = connection.root
 
+    root.projects = OOBTree()
+    
+   # print "Make your selection\n1\tAdd data\n2\tDisplay data"
+
+    print ("Enter BudgetItem Name:")
+    name = raw_input()
+
+    print ("Enter BudgetItem Description:")
+    desc = raw_input()
+
+    print ("Enter BudgetItem Quantity:")
+    quan = raw_input()
+    quan = int(quan)
+
+    print ("Enter BudgetItem Rate:")
+    rate = raw_input()
+    rate = int(rate)
+
+    item = BudgetItem (name, desc, quan, rate)
+
+    print ("Enter BudgetGroup Name:")
+    name = raw_input()
+
+    print ("Enter BudgetGroup Description:")
+    desc = raw_input()
+
+    group = BudgetGroup(name, desc)
+    group.add(item)
+
+    print ("Enter Project Name:")
+    name = raw_input()
+
+    print ("Enter Project Description:")
+    desc = raw_input()
+
+    project = Project(name, desc)
+    project.add(group)
+    
+
     # add a project to the db
-    root.projects = BTree()
-    root.projects['Project1'] = Project()
+    root.projects[project.Name] = project
 
     # commit the change to the db
     transaction.commit()
+
+    #print it out
+    for key in root.projects.keys():
+          print key + ':', root.projects[key]
+
+    #close the db
+    connection.close()
+    db.close()
+    storage.close()
--- CostEstimate/models.py
+++ CostEstimate/models.py
@@ -1,6 +1,4 @@
 from persistent import Persistent
-from BTrees.OOBTrees import BTree
-
 
 # Project.py
 # Root object
@@ -12,12 +10,28 @@ class Project(Persistent):
         self.Description = ''
         self.Bgroups['BudgetGroups'] = BTree()
 
+    def __init__(self, name, desc):
+        self.Name = name
+        self.Description = desc
+        self.GroupSet = set()
+
     def add(self, group):
-        self.Bgroups[group.Name] = group
+        self.GroupSet.add(group)
 
     def delete(self, group):
-        # write
-        print "need to write this"
+        self.GroupSet.remove (group)
+
+    # makes this object hashable so it can be used in a set
+    def __hash__(self):
+        return hash(self.Name)
+
+    #makes this object equitable so it can be hashable
+    def __eq__(self, other):
+        return self.Name == other.Name
+
+    #tostring method
+    def __str__(self):
+        return (self.Name + ": " + self.Description)
 
 
 # BudgetGroup.py
@@ -27,36 +41,73 @@ class BudgetGroup(Persistent):
     def __init__(self):
         self.Name = ''
         self.Description = ''
-        self.Bitems['BudgetItems'] = BTrees.OOBTrees()
+        self.ItemSet = set()
+
+    def __init__(self, name, desc):
+        self.Name = name
+        self.Description = desc
+        self.ItemSet = set()
 
     def add(self, item):
-        self.Bitems[item.Name] = item
+        self.ItemSet.add(item)
 
     def delete(self, item):
-        # write
-        print "need to write this"
+        self.ItemSet.remove(item)
+
+    # makes this object hashable so it can be used in a set
+    def __hash__(self):
+        return hash(self.Name)
+
+    #makes this object equitable so it can be hashable
+    def __eq__(self, other):
+        return self.Name == other.Name
+    
+    #tostring method
+    def __str__(self):
+        return (self.Name + ": " + self.Description)
 
 
 # BudgetItem.py
 # leaf object
 # Contains attributes and performs operations on them
-class BugetItem(Persistent):
+class BudgetItem(Persistent):
 
     def __init__(self):
         self.Name = ''
         self.Description = ''
-        int self.Quantity = 0
-        int self.Rate = 0
-        
+        iself.Quantity = 0
+        self.Rate = 0
 
+    def __init__(self, name, desc, quan, rate):
+        self.Name = name
+        self.Description = desc
+        self.Quantity = quan
+        self.Rate = rate
+    
+        
+    # returns the items multiplied by their rate 
     def subtotal(self):
         return self.Quantity*self.Rate
 
+    # calculates the vat on this item
     def vat(self):
         return subtotal()*0.14
 
-    def total (self)
+    # returns the subtotal plus vat
+    def total (self):
         return (subtotal+vat)
+
+    # makes this object hashable so it can be used in a set
+    def __hash__(self):
+        return hash(self.Name)
+
+    #makes this object equitable so it can be hashable
+    def __eq__(self, other):
+        return self.Name == other.Name
+
+    #tostring method
+    def __str__(self):
+        return (self.Name + ": " + self.Description)
         
 
 
